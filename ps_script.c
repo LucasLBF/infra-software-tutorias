54acb566c9074a49ccb60e0e855a01373e425fe9
Lucas Fernandes <lucas.fernandes1454@gmail.com>
2022-02-24 11:00:33 -0300

    make some small fixes

--- ps_script.c
+++ ps_script.c
@@ -1,14 +1,14 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(void)
-{
+int main(void) {
   for (int i = 0; i < 12; i++) {
     printf("%d segundo(s)\n", i + 1);
     system("ps -l | grep Z");
+    printf("\n");
     sleep(1);
   }
   return 0;
 }
-
